b2d8adf10946630ce0898513706abd0aa6d2f4bc
Fugas <77245219+Fugascz@users.noreply.github.com>
2022-01-26 20:13:19 +0100

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,14 +1,12 @@
-Citizen.CreateThread(function()
+CreateThread(function()
 	while true do
-		Citizen.Wait(0)
-        local ped = GetPlayerPed(-1)
-        local veh = GetVehiclePedIsIn(ped, false)
-        if veh ~= nil then
-            if GetVehicleClass(veh) == 18 then
-				SetVehicleEnginePowerMultiplier(GetVehiclePedIsIn(ped, false), 40.0)                
-            end
-        end
+		Wait(0)
+		local ped = PlayerPedId()
+		local veh = GetVehiclePedIsIn(ped, false)
+		if veh ~= nil then
+		    if GetVehicleClass(veh) == 18 then
+			SetVehicleEnginePowerMultiplier(GetVehiclePedIsIn(ped, false), 40.0)                
+		    end
+		end
 	end
 end)
-
-print('[BZV][PoliceBoost] Started!')
